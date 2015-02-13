77fa99c472946a55292b65690b8af4ea1443b6f8
Christine <christine.chand3@gmail.com>
2015-02-13 13:22:53 -1000

    modif

--- addons_test/easypme_notes/__openerp__.py
+++ addons_test/easypme_notes/__openerp__.py
@@ -1,22 +1,37 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 {
-    'name': "easypme_notes",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'name': "EasyPME field Notes",
 
     'description': """
         Add notes field on project form
     """,
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+    'author': "EasyPME",
+    'website': "http://www.easypme.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Project',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
@@ -25,11 +40,9 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        #'templates.xml',
         'easypme_notes_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
-        #'demo.xml',
     ],
 }
